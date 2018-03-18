bf53d1c0b3fef30fb5b7e0c626e999ed95a25227
Smokyish <smoky@kaerki.eu>
2018-03-18 16:52:31 +0200

    update stats url

--- docs/javascript/extra_js.js
+++ docs/javascript/extra_js.js
@@ -3,7 +3,7 @@
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//vps511859.ovh.net/staats/";
+    var u="//arastats.eu/staats/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
     _paq.push(['setSiteId', '1']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
